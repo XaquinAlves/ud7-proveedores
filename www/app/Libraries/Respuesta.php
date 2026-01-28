3d91c8795c14e24ba2dd2669cbd27e5d1b4c0214
XaquinAlves <xaquinalves@gmail.com>
2026-01-28 09:27:38 +0100

    Class Respuesta Finished

--- www/app/Libraries/Respuesta.php
+++ www/app/Libraries/Respuesta.php
@@ -6,24 +6,36 @@ namespace Com\Daw2\Libraries;
 
 class Respuesta
 {
-    public function __construct(
-        private int $code,
-        private ?array $data = null
-    ){}
+    private int $status;
+    private ?array $data;
 
-    public function getCode(): int
+    public function __construct(int $status)
     {
-        return $this->code;
+        if ($status >= 200 || $status < 600) {
+            $this->status = $status;
+            $this->data = null;
+        } else {
+            throw new \Exception("Status no válido");
+        }
     }
 
-    public function getData(): array
+    public function setData(?array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function getData(): ?array
     {
         return $this->data;
     }
 
-    public function hasData()
+    public function hasData(): bool
     {
         return !is_null($this->data);
     }
-
 }
